69acd5e8d2af5633117ca3989a12db840a602846
Marcin Franikowski <marcin.franikowski@silksh.pl>
2023-03-09 19:59:44 +0100

    Add target group output

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "app_fqdn" {
 output "app_fqdn_secondary" {
   value = aws_route53_record.secondary[*].fqdn
 }
+
+output "aws_lb_target_group_arn" {
+  value = aws_lb_target_group.backend.arn
+}
