66c2f6053f476887b5956120030ded9b0f7332bb
tomektom <57803202+tomektom@users.noreply.github.com>
2024-06-13 15:09:15 +0200

    fix outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "app_fqdn" {
-  value = aws_route53_record.this[0].fqdn
+  value = try(aws_route53_record.this[0].fqdn, "")
 }
 
 output "app_fqdn_secondary" {
-  value = aws_route53_record.secondary[*].fqdn
+  value = try(aws_route53_record.secondary[*].fqdn, "")
 }
 
 output "aws_lb_target_group_arn" {
-  value = aws_lb_target_group.this[0].arn
+  value = try(aws_lb_target_group.this[0].arn, "")
 }
