05ebaaef5bfe735fe15dbbc4ecd8dbee35c33111
Tomasz Warczykowski <tomasz.warczykowski@silksh.pl>
2024-10-10 11:17:39 +0200

    more outputs

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,11 @@ output "aws_lb_target_group_arn" {
 output "app_service_name" {
   value = aws_ecs_service.this.name
 }
+
+output "app_repository_url" {
+  value = var.ecr_repository_url
+}
+
+output "app_env_bucket_id" {
+  value = var.environment_bucket_id
+}
